4c79dea91ff6b44aecaf5a88c15584e7041253ac
morrishohoho <morrishohoho@qq.com>
2023-06-19 21:14:12 +0800

    Ver1.1

--- receiver/main/main.c
+++ receiver/main/main.c
@@ -38,6 +38,14 @@ int decect_header_counter = 0;
 
 int overflow_mark = 0;
 
+/*variable for vlc demodulation*/
+u_int16_t tmp_vlt = 0;
+uint8_t low_count = 0;
+uint8_t high_count = 0;
+int bit_counter = -1;
+char mes_start_flg = 0;
+/*variable for vlc demodulation*/
+
 enum CAR_STATE current_state = ENUM_STOP;
 enum CAR_STATE last_state = ENUM_STOP;
 
@@ -90,21 +98,138 @@ void decode(const char *encode, char *data)
     mes_counter++;
 }
 
+
+static void inline demodulation(const int tmp_vlt)
+{
+    if (!mes_start_flg)
+    {
+        if (tmp_vlt >= VLT_HIGH)
+        {
+            high_count++;
+        }
+        else if (tmp_vlt <= VLT_LOW)
+        {
+            if (high_count >= HEADER_THRES)
+            {
+                mes_start_flg = true;
+                high_count = 0;
+                // low_count = 0;
+                bit_counter = -1;
+                decect_header_counter++;
+                // printf("mes start!\n");
+            }
+            else
+            {
+                high_count = 0;
+                low_count = 0;
+            }
+        }
+    }
+    else
+    {
+        if (tmp_vlt >= VLT_HIGH)
+        {
+            high_count++;
+        }
+        else if (tmp_vlt <= VLT_LOW)
+        {
+            low_count++;
+        }
+
+        if (high_count != 0 && low_count != 0)
+        {
+            // dicard the lsb of header [011"0"]
+            if (bit_counter == -1)
+            {
+
+                bit_counter++;
+
+                if (low_count >= DOUBLE_LOW_THRES)
+                {
+                    mes_buffer[bit_counter] = 0;
+                    bit_counter++;
+                }
+                low_count = 0;
+            }
+            else if (bit_counter < VALID_MES_LEN)
+            {
+                if (high_count > low_count)
+                {
+                    if (high_count >= DOUBLE_HIGH_THRES)
+                    {
+                        mes_buffer[bit_counter] = 1;
+                        bit_counter++;
+                        mes_buffer[bit_counter] = 1;
+                        bit_counter++;
+                        // printf("11");
+                        high_count = 0;
+                    }
+                    else if (high_count >= SINGLE_HIGH_THRES && high_count < DOUBLE_HIGH_THRES)
+                    {
+                        mes_buffer[bit_counter] = 1;
+                        bit_counter++;
+                        // printf("1");
+                        high_count = 0;
+                    }
+                    else
+                    {
+                        mes_result[mes_counter] = 'e';
+                        mes_counter++;
+                        mes_start_flg = false;
+                        high_count = 0;
+                        low_count = 0;
+                    }
+                }
+                else
+                {
+                    if (low_count >= DOUBLE_LOW_THRES)
+                    {
+                        mes_buffer[bit_counter] = 0;
+                        bit_counter++;
+                        mes_buffer[bit_counter] = 0;
+                        bit_counter++;
+                        // printf("00");
+                        low_count = 0;
+                    }
+                    else if (low_count >= SINGLE_LOW_THRES && low_count < DOUBLE_LOW_THRES)
+                    {
+                        mes_buffer[bit_counter] = 0;
+                        bit_counter++;
+                        // printf("0");
+                        low_count = 0;
+                    }
+                    else
+                    {
+                        mes_result[mes_counter] = 'e';
+                        mes_counter++;
+                        mes_start_flg = false;
+                        low_count = 0;
+                        high_count = 0;
+                    }
+                }
+            }
+
+            // OOK Demodulation Done
+            // Codes about control car are written below
+            if (bit_counter >= VALID_MES_LEN)
+            {
+                // Decode Message
+                decode(mes_buffer, decode_data);
+                // car_control(decode_data,24);
+                mes_start_flg = false;
+                bit_counter = -1;
+                memset(mes_buffer, 0, sizeof(mes_buffer));
+                memset(decode_data, 0, sizeof(decode_data));
+            }
+        }
+    }
+}
 void app_main(void)
 {
 
     motor_config();
-
     servo_config();
     turn_forward();
-
-    /*variable for vlc*/
-    u_int16_t tmp_vlt;
-    uint8_t low_count = 0;
-    uint8_t high_count = 0;
-    int bit_counter = -1;
-    bool mes_start_flg = false;
-    /*variable for vlc*/
 
     esp_err_t ret;
     uint32_t ret_num = 0;
@@ -118,226 +243,80 @@ void app_main(void)
     ESP_ERROR_CHECK(adc_continuous_register_event_callbacks(handle, &adc1_handle, NULL));
     ESP_ERROR_CHECK(adc_continuous_start(handle));
 
-    int pwmA = 60;
-    int pwmB = 0;
-    int countA = 0;
-    int countB = 0;
-
-    bit_counter = -1;
-    mes_start_flg = false;
-    high_count = 0;
-    low_count = 0;
     memset(mes_buffer, 0, sizeof(mes_buffer));
     memset(decode_data, 0, sizeof(decode_data));
     while (1)
     {
-        while (1)
+
+        // log the demoluation result
+        if (mes_counter >= 1000)
         {
-
-            if (mes_counter >= 1000)
+            ESP_ERROR_CHECK(adc_continuous_stop(handle));
+            // printf("Start to printf Message\n");
+            // printf("Totally dectect %d messages\n", decect_header_counter);
+            bit_counter = -1;
+            mes_start_flg = false;
+            high_count = 0;
+            low_count = 0;
+            memset(mes_buffer, 0, sizeof(mes_buffer));
+            memset(decode_data, 0, sizeof(decode_data));
+            for (int i = 0; i < 100; i++)
             {
-                ESP_ERROR_CHECK(adc_continuous_stop(handle));
-                // printf("Start to printf Message\n");
-                // printf("Totally dectect %d messages\n", decect_header_counter);
-                bit_counter = -1;
-                mes_start_flg = false;
-                high_count = 0;
-                low_count = 0;
-                memset(mes_buffer, 0, sizeof(mes_buffer));
-                memset(decode_data, 0, sizeof(decode_data));
-                for (int i = 0; i < 100; i++)
-                {
-                    printf("%c\n", mes_result[i]);
-                }
-                mes_counter = 0;
-                ESP_ERROR_CHECK(adc_continuous_start(handle));
+                printf("%c\n", mes_result[i]);
             }
-            ret = adc_continuous_read(handle, result, EXAMPLE_READ_LEN, &ret_num, 0);
-            if (overflow_mark == 1)
-            {
-                printf("overflow\n");
-                bit_counter = -1;
-                mes_start_flg = false;
-                high_count = 0;
-                low_count = 0;
-                memset(mes_buffer, 0, sizeof(mes_buffer));
-                memset(decode_data, 0, sizeof(decode_data));
-                overflow_mark = 0;
-            }
-            if (ret == ESP_OK)
-            {
-
-                // ESP_LOGI("TASK", "ret is %x, ret_num is %" PRIu32, ret, ret_num);?
-                // ESP_ERROR_CHECK(adc_continuous_stop(handle));
-
-                int adc_inverse = 0;
-                int tmp_adc[2] = {0};
-                for (int i = 0; i < ret_num; i += SOC_ADC_DIGI_RESULT_BYTES)
-                {
-                    adc_digi_output_data_t *p = (void *)&result[i];
-                    if (check_valid_data(p))
-                    {
-                        tmp_adc[adc_inverse] = p->type1.data;
-                        adc_inverse++;
-                        if (adc_inverse == 2)
-                        {
-                            for (int tmpX = 1; tmpX >= 0; tmpX--)
-                            {
-                                tmp_vlt = tmp_adc[tmpX];
-                                // printf("%d\n\r", tmp_vlt);
-                                if (!mes_start_flg)
-                                {
-                                    if (tmp_vlt >= VLT_HIGH)
-                                    {
-                                        high_count++;
-                                    }
-                                    else if (tmp_vlt <= VLT_LOW)
-                                    {
-                                        if (high_count >= HEADER_THRES)
-                                        {
-                                            mes_start_flg = true;
-                                            high_count = 0;
-                                            // low_count = 0;
-                                            bit_counter = -1;
-                                            decect_header_counter++;
-                                            // printf("mes start!\n");
-                                        }
-                                        else
-                                        {
-                                            high_count = 0;
-                                            low_count = 0;
-                                        }
-                                    }
-
-                                }
-                                else
-                                {
-                                    if (tmp_vlt >= VLT_HIGH)
-                                    {
-                                        high_count++;
-                                    }
-                                    else if (tmp_vlt <= VLT_LOW)
-                                    {
-                                        low_count++;
-                                    }
-
-                                    if (high_count != 0 && low_count != 0)
-                                    {
-                                        // dicard the lsb of header [011"0"]
-                                        if (bit_counter == -1)
-                                        {
-
-                                            bit_counter++;
-
-                                            if (low_count >= DOUBLE_LOW_THRES)
-                                            {
-                                                mes_buffer[bit_counter] = 0;
-                                                bit_counter++;
-                                            }
-                                            low_count = 0;
-                                        }
-                                        else if (bit_counter < VALID_MES_LEN)
-                                        {
-                                            if (high_count > low_count)
-                                            {
-                                                if (high_count >= DOUBLE_HIGH_THRES)
-                                                {
-                                                    mes_buffer[bit_counter] = 1;
-                                                    bit_counter++;
-                                                    mes_buffer[bit_counter] = 1;
-                                                    bit_counter++;
-                                                    // printf("11");
-                                                    high_count = 0;
-                                                }
-                                                else if (high_count >= SINGLE_HIGH_THRES && high_count < DOUBLE_HIGH_THRES)
-                                                {
-                                                    mes_buffer[bit_counter] = 1;
-                                                    bit_counter++;
-                                                    // printf("1");
-                                                    high_count = 0;
-                                                }
-                                                else
-                                                {
-                                                    mes_result[mes_counter] = 'e';
-                                                    mes_counter++;
-                                                    mes_start_flg = false;
-                                                    high_count = 0;
-                                                    low_count = 0;
-                                                }
-                                            }
-                                            else
-                                            {
-                                                if (low_count >= DOUBLE_LOW_THRES)
-                                                {
-                                                    mes_buffer[bit_counter] = 0;
-                                                    bit_counter++;
-                                                    mes_buffer[bit_counter] = 0;
-                                                    bit_counter++;
-                                                    // printf("00");
-                                                    low_count = 0;
-                                                }
-                                                else if (low_count >= SINGLE_LOW_THRES && low_count < DOUBLE_LOW_THRES)
-                                                {
-                                                    mes_buffer[bit_counter] = 0;
-                                                    bit_counter++;
-                                                    // printf("0");
-                                                    low_count = 0;
-                                                }
-                                                else
-                                                {
-                                                    mes_result[mes_counter] = 'e';
-                                                    mes_counter++;
-                                                    mes_start_flg = false;
-                                                    low_count = 0;
-                                                    high_count = 0;
-                                                }
-                                            }
-                                        }
-
-                                        // OOK Demodulation Done
-                                        // Codes about control car are written below
-                                        if (bit_counter >= VALID_MES_LEN)
-                                        {
-
-                                            // printf("\n");
-                                            // printf("oringal!\n");
-                                            // Decode Message
-                                            decode(mes_buffer, decode_data);
-                                            // if (last_state != current_state)
-                                            // {
-                                            //     last_state = current_state;
-                                            //     car_control(decode_data,25);
-                                            // }
-                                            // car_control(decode_data,24);
-                                            mes_start_flg = false;
-                                            bit_counter = -1;
-                                            memset(mes_buffer, 0, sizeof(mes_buffer));
-                                            memset(decode_data, 0, sizeof(decode_data));
-                                        }
-                                    }
-                                }
-                            }
-                            adc_inverse = 0;
-                        }
-                    }
-                    /**
-                     * Because printing is slow, so every time you call `ulTaskNotifyTake`, it will immediately return.
-                     * To avoid a task watchdog timeout, add a delay here. When you replace the way you process the data,
-                     * usually you don't need this delay (as this task will block for a while).
-                     */
-                    // vTaskDelay(1);
-                    else if (ret == ESP_ERR_TIMEOUT)
-                    {
-                        // We try to read `EXAMPLE_READ_LEN` until API returns timeout, which means there's no available data
-                        break;
-                    }
-                }
-                // ESP_ERROR_CHECK(adc_continuous_start(handle));
-            }
-            //  vTaskDelay(1);
+            mes_counter = 0;
+            ESP_ERROR_CHECK(adc_continuous_start(handle));
         }
 
-        ESP_ERROR_CHECK(adc_continuous_stop(handle));
-        ESP_ERROR_CHECK(adc_continuous_deinit(handle));
+        ret = adc_continuous_read(handle, result, EXAMPLE_READ_LEN, &ret_num, 0);
+
+        // detect the overflow
+        if (overflow_mark == 1)
+        {
+            printf("overflow\n");
+            bit_counter = -1;
+            mes_start_flg = false;
+            high_count = 0;
+            low_count = 0;
+            memset(mes_buffer, 0, sizeof(mes_buffer));
+            memset(decode_data, 0, sizeof(decode_data));
+            overflow_mark = 0;
+        }
+
+        // if the adc read is successful
+        if (ret == ESP_OK)
+        {
+
+            // ESP_LOGI("TASK", "ret is %x, ret_num is %" PRIu32, ret, ret_num);?
+            // ESP_ERROR_CHECK(adc_continuous_stop(handle));
+
+            int adc_inverse = 0;
+            int tmp_adc[2] = {0};
+            for (int i = 0; i < ret_num; i += SOC_ADC_DIGI_RESULT_BYTES)
+            {
+                adc_digi_output_data_t *p = (void *)&result[i];
+                if (check_valid_data(p))
+                {
+                    tmp_adc[adc_inverse] = p->type1.data;
+                    adc_inverse++;
+                    if (adc_inverse == 2)
+                    {
+                        for (int tmpX = 1; tmpX >= 0; tmpX--)
+                        {
+                            tmp_vlt = tmp_adc[tmpX];
+                            // printf("%d\n\r", tmp_vlt);
+                            demodulation(tmp_vlt);
+                        }
+                        adc_inverse = 0;
+                    }
+                }
+                else if (ret == ESP_ERR_TIMEOUT)
+                {
+                    // We try to read `EXAMPLE_READ_LEN` until API returns timeout, which means there's no available data
+                    break;
+                }
+            }
+            // ESP_ERROR_CHECK(adc_continuous_start(handle));
+        }
     }
 }
